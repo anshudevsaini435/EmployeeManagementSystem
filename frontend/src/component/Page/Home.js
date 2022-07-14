752c3c49c24f9f6331330d2dbb9e8a937bd9109b
tusharchaudhari30 <blazeric30@gmail.com>
2022-07-14 17:47:46 +0530

    Top nav has been added with button.

--- frontend/src/component/Page/Home.js
+++ frontend/src/component/Page/Home.js
@@ -8,11 +8,12 @@ class Home extends Component {
             <div className="sm:ml-56">
                 <div className="fixed w-full bg-white"><TopNav/></div>
                 <div className="flex flex-wrap pt-20">
-                    <div className="lg:w-1/3 md:w-1/2 w-full p-5"><Card><p className="font-sans font-semibold">Hello world !!</p></Card></div>
-                    <div className="lg:w-1/3 md:w-1/2 w-full p-5"><Card/></div>
-                    <div className="lg:w-1/3 md:w-1/2 w-full p-5"><Card/></div>
-                    <div className="lg:w-1/2 md:w-1/2 w-full p-5"><Card/></div>
-                    <div className="lg:w-1/2 md:w-1/2 w-full p-5"><Card/></div>
+                    <div className="lg:w-1/4 md:w-1/2 w-full p-5"><Card><p className="font-sans font-semibold">Total no of Employee</p></Card></div>
+                    <div className="lg:w-1/4 md:w-1/2 w-full p-5"><Card><p className="font-sans font-semibold">Projects Completed</p></Card></div>
+                    <div className="lg:w-1/4 md:w-1/2 w-full p-5"><Card><p className="font-sans font-semibold">Active Projects</p></Card></div>
+                    <div className="lg:w-1/4 md:w-1/2 w-full p-5"><Card><p className="font-sans font-semibold"></p></Card></div>
+                    <div className="lg:w-1/2 md:w-1/2 w-full p-5"><Card><p className="font-sans font-semibold"></p></Card></div>
+                    <div className="lg:w-1/2 md:w-1/2 w-full p-5"><Card><p className="font-sans font-semibold"></p></Card></div>
                 </div>
             </div>
         );
